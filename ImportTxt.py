9cad74f9a215f68cd7e2ded1ed9e974e8a822c72
Filipe Junqueira <filipelqj@gmail.com>
2018-08-05 21:44:56 +0100

    hello

--- ImportTxt.py
+++ ImportTxt.py
@@ -1,27 +1,38 @@
 import csv
 import numpy as np
 
-#fnameString = 'D:\PhD\Code\TraceNN\Data\default_2017Jul24-185510_STM-STM_AtomManipulation--'
-#restString = '_1-I(r).txt'
-#for i in range (3,893):
-#    fname = fnameString + str(i) + restString
+fnameString = 'D:\PhD\Code\TraceNN\Data\default_2017Jul24-185510_STM-STM_AtomManipulation--'
+restString = '_1-I(r).txt'
 
-
+ncurves = 889
+npoints = 1000
+raw_data = np.zeros( [2*ncurves+1), npoints )
+fname = 'Test.txt'
 def open_trace_files(fname):
-    with open(fname, 'r') as f:
+
+with open('Test.txt', 'r') as f:
     content = f.readlines()
     prefixes = ('#')
-
     for word in content[:]:
-    if word.startswith(prefixes):
-        content.remove(word)
-        content = ''.join(content)
-        content = content.split()
+        if word.startswith(prefixes):
+            content.remove(word)
+
+    content = ''.join(content)
+    content = content.split()
 
     localsize = int(float(len(content))/2)
     localdatax = np.zeros(localsize)
     localdatay = np.zeros(localsize)
 
     for i in range(0,localsize):
-    localdatax[i] = float(content[2*i])
-    localdatay[i] = float(content[2*i+1])
+        localdatax[i] = float(content[2*i])
+        localdatay[i] = float(content[2*i+1])
+
+print(localdatax)
+#return localdatax, localdatay
+
+#for i in range (3,893):
+#    filename = fnameString + str(i) + restString
+#    open_trace_files(filename)
+#    raw_data[:,2*i+1] = localdatax[i]
+#    raw_data[:,2*i+1] = localdatay[i]
---
+++ Main_traces.py
@@ -0,0 +1,15 @@
+import csv
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import array
+import ImportTxt
+
+
+
+
+
+#fnameString = 'D:\PhD\Code\TraceNN\Data\default_2017Jul24-185510_STM-STM_AtomManipulation--'
+#restString = '_1-I(r).txt'
+#for i in range (3,893):
+#    fname = fnameString + str(i) + restString
