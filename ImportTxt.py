b66fa656d63e03d3d965d13538429f3b33735fbc
Filipe Junqueira <filipelqj@gmail.com>
2018-08-14 17:34:24 +0100

    14/08/2018 commit

--- ImportTxt.py
+++ ImportTxt.py
@@ -19,38 +19,9 @@ def open_trace_files(fname,new_length):
         localdatax = np.zeros(localsize)
         localdatay = np.zeros(localsize)
 
-
-        #Now im going to resize the value of localdatax if localsize is not new_length
-        #If localsize is diferent I'll have to resize it
-        #This means create a array with new lenght from localdatax and another one from localdatay
-        '''
-        if localsize != new_length:
-
-            aux = np.linspace(min(localdatax), max(localdatax) , num = new_length)
-
-            new_localdatax = np.zeros(new_length)
-            new_localdatay = np.zeros(new_length)
-
-            sply = UnivariateSpline(localdatax, localdatay)
-            splx = UnivariateSpline(localdatax, localdatax)
-
-            new_localdatay = splx()
-            '''
         for i in range(0,localsize):
             localdatax[i] = float(content[2*i])
             localdatay[i] = float(content[2*i+1])
 
 
     return localdatax, localdatay
-
-
-"""
-
-    #NEED TO INTERPOLATE DATA WHERE  THE VALUE IS NOT 1000
-        old_indices = np.arange(0,len(a))
-        new_length = 11
-        new_indices = np.linspace(0,len(a)-1,new_length)
-        spl = UnivariateSpline(old_indices,a,k=3,s=0)
-        new_array = spl(new_indices)
-
-"""
