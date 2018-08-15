63ef8dcd347dc6983a81e11c908d8c7c29ee6d0d
Filipe Junqueira <filipelqj@gmail.com>
2018-08-15 21:31:46 +0100

    15-08-2018 Desktop

--- FFT_of_Array.py
+++ FFT_of_Array.py
@@ -5,29 +5,23 @@ import matplotlib.pyplot as plt
 from numpy import array
 from numpy import random
 
-N = 1000000
-
+N = 1024
 #Generating a Synthetic Signal
 # linspace creates a lineared spaced vector (goes from 0 to 2pi in 1000 points)
-t = np.linspace(0, 2*np.pi, N, endpoint=True)
-A = 100.0 # Amplitude in Unit
-fo = 88
+t = np.linspace(0,2*2*np.pi, N, endpoint=True)
+A = 1000 # Amplitude in Unit
+fo = 3
 #s is now a vector of 1000 points since t has 1000 points
 
 #bigdata = np.zeros((tsize,Nsignal))
 #print(bigdata)
-s = np.sin(t*2*np.pi*fo)
-
-plt.plot(t,s)
-plt.xlabel('Time ($s$)')
-plt.ylabel('Amplitude ($Unit$)')
-plt.show()
+s = A*np.sin(t*2*np.pi*fo)
 
 ################  Fucking-Chuck-Norris-Fast-FFT   #############################
-#Now we are going to create a Fourier Transform (of variable
+#Now we are going to create a Fourier Transform of variable s
 Y = np.fft.fft(s)
-N = len(Y)/2 + 1
-N = int(N)
+print(len(Y))
+fft_size = int(len(Y)/2 + 1)
 
 #Now we need to adjust the frequency and the Amplitude axis
 #Stating with the Frequency Axix (x-Axis)
@@ -39,14 +33,19 @@ print('fa=%.2fHz (Frequency)' % fa)
 
 #Building Nyquist-Shannon-Frequency from the Nyquist-Shannon Sampling Theorem
 
-X = np.linspace(0, fa/2, N, endpoint=True)
+X = np.linspace(0, fa/2, fft_size, endpoint=True)
+
 
 # Correcting Wrong Amplitude Spectrum because of Leakage Effect
 # We need a window function to get a periodic signal from real data
 # you could use np.hanning, np.hamming or np.blackman for instance
 
-hann = np.hanning(len(s))
+hann = np.hamming(len(s))
 Yhann = np.fft.fft(hann*s)
+
+
+Y_FFT = 2.0*np.abs(Yhann[:fft_size])/fft_size
+print(max(Y_FFT))
 
 plt.figure(figsize=(7,3))
 plt.subplot(121)
@@ -57,9 +56,10 @@ plt.xlabel('Time ($s$)')
 plt.ylabel('Amplitude ($Unit$)')
 
 plt.subplot(122)
-plt.plot(X, 2.0*np.abs(Yhann[:N])/N)
+plt.plot(X, 2.0*np.abs(Yhann[:fft_size])/fft_size)
 plt.title('Frequency Domain Signal')
 plt.xlim(0,120)
+plt.ylim(0,1200)
 plt.xlabel('Frequency ($Hz$)')
 plt.ylabel('Amplitude ($Unit$)')
 
